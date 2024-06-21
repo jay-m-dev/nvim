b2c737b2e5c48b1d1c8d69ff04ddd44bfbb343cc
Jay Moran <jay-moran@outlook.com>
2024-06-21 10:44:27 -0700

    new init.lua

---
+++ init.lua
@@ -0,0 +1,79 @@
+-- init.lua
+
+-- Load packer.nvim
+vim.cmd [[packadd packer.nvim]]
+
+-- Set leader key
+vim.g.mapleader = ' '
+
+-- Key mappings
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>v', ':vs<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>s', ':sp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':!python3 %<CR>', { noremap = true, silent = true })
+
+-- Set options
+vim.o.number = true         -- Show line numbers
+vim.o.expandtab = true      -- Use spaces instead of tabs
+vim.o.shiftwidth = 4        -- Number of spaces for indentation
+vim.o.tabstop = 4           -- Number of spaces per tab
+vim.o.softtabstop = 4       -- Number of spaces per tab in insert mode
+
+-- Initialize packer
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/vim-vsnip'
+  use 'hrsh7th/cmp-vsnip'
+
+  -- Add Comment.nvim plugin
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+  -- Add other plugins here
+end)
+
+-- nvim-cmp setup
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+  }
+})
+
+-- lspconfig
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+lspconfig.pyright.setup {
+  capabilities = capabilities,
+}
+
+-- Keybinding for hover documentation
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+
