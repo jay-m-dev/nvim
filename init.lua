894c93a47d139026ed92c8cb3cfa701914fb017e
Jay Moran <jay-moran@outlook.com>
2024-07-08 13:49:03 -0700

    Add quit all

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.g.mapleader = ' '
 -- Key mappings
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><S-Q>', ':qa<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
