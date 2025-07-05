c6ca2627ec5a3f4f06afa20d7a0b37cdf03bec14
Jay Moran <jay-moran@outlook.com>
2025-07-04 21:43:04 -0700

    Config sonnet on copilotchat

--- init.lua
+++ init.lua
@@ -20,6 +20,14 @@ require("lazy").setup({
   "hrsh7th/vim-vsnip",
   "hrsh7th/cmp-vsnip",
   {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<CR>", desc = "Open LazyGit" },
+    },
+  },
+  {
     "folke/which-key.nvim",
     config = function()
       require("which-key").setup {}
@@ -31,6 +39,11 @@ require("lazy").setup({
     config = function()
       require("mini.icons").setup()
     end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+    opts = {},
   },
   {
     "glepnir/lspsaga.nvim",
@@ -86,13 +99,14 @@ require("lazy").setup({
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
     },
     opts = {
       show_help = "yes",
+      model = "claude-sonnet-4"
     },
     keys = {
       { "<leader>cc", function() require("CopilotChat").toggle() end, desc = "Toggle Copilot Chat" },
@@ -220,6 +234,7 @@ map('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', opts)
 map('n', '<Leader>fb', '<cmd>Telescope buffers<CR>', opts)
 map('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>', opts)
 map('n', '<Leader>dd', '<cmd>Telescope diagnostics<CR>', opts)
+map('n', '<Leader>gw', ':GptWrap ', opts)
 
 -- Diagnostic float on hover
 vim.cmd [[
@@ -274,36 +289,53 @@ vim.cmd [[
   highlight DiffText   ctermbg=58 guibg=#5c3566
 ]]
 
--- Which-Key descriptions
-require("which-key").register({
-  ["<leader>"] = {
-    Q = "Quit all",
-    cc = "Copilot Chat toggle",
-    ce = "Copilot Chat explain code",
-    dd = "Diagnostics",
-    fb = "Buffers",
-    ff = "Find files",
-    fg = "Live grep",
-    fh = "Help tags",
-    h = "Window left",
-    j = "Window down",
-    k = "Window up",
-    l = "Window right",
-    mp = "Markdown preview start",
-    ms = "Markdown preview stop",
-    mt = "Markview toggle",
-    q = "Quit",
-    r = "Run python file",
-    s = "Horizontal split",
-    ts = "Terminal horizontal split",
-    tv = "Terminal vertical split",
-    v = "Vertical split",
-    w = "Save file",
-    p = "Paste from + (strip \\r)",
-  }
-})
+-- -- Which-Key descriptions
+-- local wk = require("which-key")
+-- wk.register({
+--   { "<leader>cc", desc = "Copilot Chat toggle" },
+--   { "<leader>ce", desc = "Copilot Chat explain code" },
+--   { "<leader>dd", desc = "Diagnostics" },
+--   { "<leader>fb", desc = "Buffers" },
+--   { "<leader>ff", desc = "Find files" },
+--   { "<leader>fg", desc = "Live grep" },
+--   { "<leader>fh", desc = "Help tags" },
+--   { "<leader>h", desc = "Window left" },
+--   { "<leader>j", desc = "Window down" },
+--   { "<leader>k", desc = "Window up" },
+--   { "<leader>l", desc = "Window right" },
+--   { "<leader>mp", desc = "Markdown preview start" },
+--   { "<leader>ms", desc = "Markdown preview stop" },
+--   { "<leader>mt", desc = "Markview toggle" },
+--   { "<leader>p", desc = "Paste from + (strip \\r)" },
+--   { "<leader>q", desc = "Quit" },
+--   { "<leader>r", desc = "Run python file" },
+--   { "<leader>s", desc = "Horizontal split" },
+--   { "<leader>ts", desc = "Terminal horizontal split" },
+--   { "<leader>tv", desc = "Terminal vertical split" },
+--   { "<leader>v", desc = "Vertical split" },
+--   { "<leader>w", desc = "Save file" },
+-- })
 
 vim.keymap.set('t', 'jk', '<C-\\><C-n>', { noremap = true })
 
 vim.g.python3_host_prog = vim.fn.expand("~/.venvs/nvim/bin/python")
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "octo", "octo_issue", "octo_pr" },
+  callback = function()
+    local opts = { buffer = true, noremap = true, silent = true }
+    vim.keymap.set("n", "e", "<cmd>OctoEdit<CR>", opts)
+  end,
+})
+
+vim.api.nvim_create_user_command("GptWrap", function(opts)
+  local args = opts.fargs
+  if #args == 0 then
+    args = { vim.fn.expand("%:p") }
+  end
+  vim.fn.jobstart(vim.list_extend({ "gptwrap" }, args), { detach = true })
+end, {
+  nargs = "*",
+  complete = "file",
+  desc = "Wrap file(s) in triple backticks and open ChatGPT"
+})
